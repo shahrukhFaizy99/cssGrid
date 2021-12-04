ebb1411c0f30f5cfe78c96b13895bbf007a7b794
shahrukhFaizy99 <shahrukhfaizy17@gmail.com>
2021-12-04 12:58:43 +0500

    css-grid

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Grid from "./Components/Grid";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Grid.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Grid = () => {
+  return (
+    <>
+      <div className="main-heading">
+        <h1>Grid Layout</h1>
+      </div>
+      <main>
+        <div className="div1">header</div>
+        <div className="div2">box1</div>
+        <div className="div3">box2</div>
+        <div className="div4">box3</div>
+        <div className="div5">sidebar</div>
+        <div className="div6">main-content</div>
+        <div className="div7">footer</div>
+      </main>
+    </>
+  );
+};
+
+export default Grid;
